826a398ce202899b2938f8e472f8af0d2a53ee2d
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-28 15:52:38 +0100

    UpdateController

--- app/Http/Controllers/DeployNotificationController.php
+++ app/Http/Controllers/DeployNotificationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Contracts\AMQPConnectionInterface;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use PhpAmqpLib\Message\AMQPMessage;
 
 class DeployNotificationController extends Controller
@@ -13,17 +12,13 @@ class DeployNotificationController extends Controller
     {
         $payload = json_decode($request->payload);
 
-        Log::info($payload->state);
         $amqpConnection = app()->make(AMQPConnectionInterface::class);
         $channel = $amqpConnection->channel();
         $channel->queue_declare('travisStatus', false, false, false, false);
 
         $msg = new AMQPMessage($payload->state);
 
-//        for($i = 0; $i < 100; $i++) {
         $channel->basic_publish($msg, '', 'travisStatus');
-//            sleep(1);
-//        }
 
         return $request->all();
     }

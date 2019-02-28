edc54952f290365f5576dfec245dd909ead294c8
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-28 15:55:29 +0100

    UpdateController

--- config/amqp.php
+++ config/amqp.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'host'      => env('AMQP_HOST', '127.0.0.1'),
-    'username'  => env('AMQP_USER', ''),
+    'username'  => env('AMQP_USER', 'homestead'),
     'password'  => env('AMQP_PASSWORD', 'homestead'),
     'port'      => env('AMQP_PORT', 5672)
 ];
